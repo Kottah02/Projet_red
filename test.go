b5add939fd8a604329bf8d867cbf13850e78ef73
Xerly <xerly.ji@ynov.com>
2024-09-18 15:00:21 +0200

    test1

--- test.go
+++ test.go
@@ -212,6 +212,10 @@ func gatherResources(player *Player) {
 		fmt.Println(yellow + "1" + reset + " - Bois")
 		fmt.Println(yellow + "2" + reset + " - Pierre")
 		fmt.Println(yellow + "3" + reset + " - Feuilles")
+		fmt.Println(yellow + "4" + reset + " - Fourrure de Loup")
+		fmt.Println(yellow + "5" + reset + " - Peau de Troll")
+		fmt.Println(yellow + "6" + reset + " - Cuir de Sanglier")
+		fmt.Println(yellow + "7" + reset + " - Plume de Corbeau")
 		fmt.Println(yellow + "0" + reset + " - Retour")
 		fmt.Println(cyan + "================================================" + reset)
 
@@ -235,6 +239,23 @@ func gatherResources(player *Player) {
 			leaf := rand.Intn(10) + 1
 			player.Inventaire.Leaf += leaf
 			fmt.Printf(green+"Vous avez récolté %d feuilles. Total de feuilles : %d\n"+reset, leaf, player.Inventaire.Leaf)
+		case 4:
+			Fourrure := rand.Intn(10) + 1
+			player.Inventaire.Fourrure += Fourrure
+			fmt.Printf(green+"Vous avez récolté %d feuilles. Total de Fourrure de Loup : %d\n"+reset, Fourrure, player.Inventaire.Fourrure)
+
+		case 5:
+			PeauTroll := rand.Intn(10) + 1
+			player.Inventaire.Peau_Troll += PeauTroll
+			fmt.Printf(green+"Vous avez récolté %d feuilles. Total de Peau de Troll : %d\n"+reset, PeauTroll, player.Inventaire.Peau_Troll)
+		case 6:
+			CuirSanglier := rand.Intn(10) + 1
+			player.Inventaire.CuirSanglier += CuirSanglier
+			fmt.Printf(green+"Vous avez récolté %d feuilles. Total de Cuir de Sanglier : %d\n"+reset, CuirSanglier, player.Inventaire.CuirSanglier)
+		case 7:
+			Plume_de_Corbeau := rand.Intn(10) + 1
+			player.Inventaire.PlumeCorbeau += Plume_de_Corbeau
+			fmt.Printf(green+"Vous avez récolté %d feuilles. Total de Plume de Corbeau : %d\n"+reset, Plume_de_Corbeau, player.Inventaire.PlumeCorbeau)
 		default:
 			fmt.Println(red + "Choix invalide." + reset)
 		}
